080de45bc1981748f417c466255816f284bfe936
shivamsy <shivamsingh.y16@iiits.in>
2019-01-03 19:13:05 +0530

    Added error messages. Added hideSoftInput. Final Commit

--- app/src/main/java/com/example/shivam/guesstheweather/MainActivity.java
+++ app/src/main/java/com/example/shivam/guesstheweather/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,9 +19,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView city, weather;
+    Button find;
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -53,22 +62,38 @@ public class MainActivity extends AppCompatActivity {
                 String weatherInfo = object.getString("weather");
 
                 JSONArray array = new JSONArray(weatherInfo);
-                System.out.println(array.length());
+                System.out.println(weatherInfo);
 
                 for (int i=0; i<array.length(); i++) {
                     JSONObject jsonPart = array.getJSONObject(i);
 
+                    weather.setText(jsonPart.getString("main")+" : "+jsonPart.getString("description")+"\r\n");
+
                     Log.i("main: ", jsonPart.getString("main"));
                     Log.i("desc: ", jsonPart.getString("description"));
                 }
-                System.out.println(array.getJSONObject(0).getString("main"));
 
             } catch (Exception e) {
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Unable to find weather!", Toast.LENGTH_SHORT).show();
             }
-
-
         }
+    }
+
+    public void findWeather(View view) {
+
+        DownloadTask task = new DownloadTask();
+        InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        manager.hideSoftInputFromWindow(weather.getWindowToken(), 0);
+
+        Log.i("city: ", city.getText().toString());
+
+        try {
+                String cty = URLEncoder.encode(city.getText().toString(), "UTF-8");
+                task.execute("https://openweathermap.org/data/2.5/weather?q=" + cty + "&appid=b6907d289e10d714a6e88b30761fae22");
+        }catch(Exception e){
+            e.printStackTrace();
+            }
     }
 
     @Override
@@ -76,11 +101,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadTask task = new DownloadTask();
-        try {
-            task.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        city = findViewById(R.id.city);
+        weather = findViewById(R.id.weather);
     }
 }
